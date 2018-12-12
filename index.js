fcd6216e917a455c7bb70100c47799f685f1d9d0
Isabel <iarcones@gmail.com>
2018-12-11 23:45:15 -0800

    first init files

---
+++ index.js
@@ -0,0 +1,30 @@
+var inquirer = require("inquirer");
+
+var Letter = require("./Letter");
+
+
+
+var word = "abcde";
+var letter = "a"
+
+
+
+
+// inquirer
+//   .prompt([
+//     // Here we create a basic text prompt.
+//     {
+//       type: "input",
+//       message: "letter",
+//       name: "letterClicked"
+//     }
+// ])
+// .then(function(inquirerResponse) {
+
+//   if (inquirerResponse.confirm) {
+    
+//   }
+//   else {
+//     console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+//   }
+// });
